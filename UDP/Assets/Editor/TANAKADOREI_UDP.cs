4ced7764b4a53167989dcf5156d5b8831bfeb477
TANAKADOREI <tanaka.dorei.game.ch@gmail.com>
2024-06-23 18:23:11 +0900

    Update TANAKADOREI_UDP.cs

--- UDP/Assets/Editor/TANAKADOREI_UDP.cs
+++ UDP/Assets/Editor/TANAKADOREI_UDP.cs
@@ -5,4 +5,16 @@ using UnityEngine;
 
 public class TANAKADOREI_UDP : EditorWindow
 {
+	public static void ShowWindow()
+	{
+		GetWindow<TANAKADOREI_UDP>("UDP");
+	}
+
+	void OnGUI()
+	{
+		var time_progress_rect = EditorGUILayout.GetControlRect(false,24);
+		var my_goal_progress_rect = EditorGUILayout.GetControlRect(false,24);
+
+		EditorGUI.DrawRect()
+	}
 }
